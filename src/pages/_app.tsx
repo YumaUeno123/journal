631e0519c2ac5ced5ea9f395d3b75d6b8f1e1edf
ueno.yuma <ueno.yuma@moneyforward.co.jp>
2022-01-18 20:09:21 +0900

    Configure mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from '@/styles/theme';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { createEmotionCache } from '@/lib/createEmotionCache';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
+
+type MyAppProps = AppProps & {
+  emotionCache?: EmotionCache;
+};
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp;
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  // TODO: 設定は随時追加する
+  typography: {
+    fontFamily: ['Noto Sans JP', 'sans-serif'].join(','),
+  },
+});
