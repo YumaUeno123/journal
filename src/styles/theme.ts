3d8bbb43e014b00771dba0a7fdee7a336dfc11ba
ueno.yuma <ueno.yuma@moneyforward.co.jp>
2022-01-19 00:41:26 +0900

    Update theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,6 +16,43 @@ export const theme = createTheme({
   },
   typography: {
     fontFamily: ['Noto Sans JP', 'sans-serif'].join(','),
-    fontSize: 12,
+    htmlFontSize: 16, // Mui default
+    fontSize: 14, // Mui default
+    h4: {
+      fontSize: '1.5rem', // 24px
+    },
+    h5: {
+      fontSize: '1.125rem', // 18px
+    },
+    h6: {
+      fontSize: '1rem', // 16px
+    },
+    subtitle1: {
+      fontSize: '0.875rem', // 14px
+    },
+    subtitle2: {
+      fontSize: '0.8125rem', // 13px
+    },
+    body1: {
+      fontSize: '0.8125rem', // 13px
+    },
+    body2: {
+      fontSize: '0.875rem', // 14px
+    },
+    button: {
+      fontSize: '0.8125rem', // 13px
+    },
+    caption: {
+      fontSize: '0.75rem', // 12px
+    },
+    overline: {
+      fontSize: '0.625rem', // 10px
+    },
   },
+  mixins: {
+    toolbar: {
+      minHeight: 42,
+    },
+  },
+  spacing: [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40],
 });
